fa587a50ae3c578c12d354610822825fb2405e99
Simon Magus <simonmagusgno@gmail.com>
2023-04-20 09:02:12 +0200

    Change to httpx and selectolax

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+import httpx
+from selectolax.parser import HTMLParser
 
 
 def get_data(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    h1_elements = soup.find_all('h1')
+    response = httpx.get(url)
+    parser = HTMLParser(response.content)
+    h1_elements = parser.css('h1')
     for h1 in h1_elements:
-        print(h1.text)
-    a_elements = soup.find_all('a')
+        print(h1.text())
+    a_elements = parser.css('a')
     for a in a_elements:
-        print(a.get('href'))
+        print(a.attributes['href'])
 
 
 url = 'https://www.example.com'
